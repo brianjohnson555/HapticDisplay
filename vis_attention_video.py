2ca2516c22f22e70f9e7f115006dc3b9acd32305
Brian Johnson <brjo8465@colorado.edu>
2023-09-18 10:26:26 +0200

    old updates

--- vis_attention_video.py
+++ vis_attention_video.py
@@ -18,7 +18,7 @@ from time import sleep
 device = torch.device("cpu")
 
 #model = timm.create_model('vit_small_patch16_224_dino',pretrained=True)
-model = torch.hub.load('facebookresearch/dino:main','dino_vits16')
+model = torch.hub.load('facebookresearch/dino:main','dino_vits8')
 
 
 # Load MiDaS model onto CPU
@@ -31,11 +31,12 @@ transform = midas_transforms.small_transform
 
 # Setup frame capture
 cap = cv2.VideoCapture('video3.mp4') #use video
-# cap = cv2.VideoCapture(0) #stream from webcam
+#cap = cv2.VideoCapture(0) #stream from webcam
 previous_frame = None
 
 frames_list = []
 attentions_list = []
+depth_list = []
 
 while True: 
     # Load frame
@@ -48,38 +49,24 @@ while True:
     # img = cv2.addWeighted(img,1.5,img,0,1)
     
     image = Image.fromarray(img)
-    Tx = transforms.Resize((50*9,50*16))(image)
+    Tx = transforms.Resize((10*9,10*16))(image)
     Tx2 = transforms.ToTensor()(Tx).unsqueeze_(0)
     Tx3 = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(Tx2)
     Tx3.requires_grad = True
 
-    # # Compute depth
-    # input_batch = transform(img).to(device)
-    # with torch.no_grad():
-    #     prediction = midas(input_batch)
-
-    #     prediction = torch.nn.functional.interpolate(
-    #         prediction.unsqueeze(1),
-    #         size=(2*90, 2*160),
-    #         mode="bicubic",
-    #         align_corners=False,
-    #     ).squeeze()
-    # output = prediction.cpu().numpy()
-    # depth = cv2.normalize(output, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
-    # #depth = (depth).astype(np.uint8)
     model.eval()
     model.to(device)
 
-    # start_time = time.time()
+    start_time = time.time()
     attentions = model.get_last_selfattention(Tx3)
-    # print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time))
     
     nh = attentions.shape[1]
     attentions = attentions[0, :, 0, 1:].reshape(nh,-1)
     val, idx = torch.sort(attentions)
     val /= torch.sum(val, dim=1, keepdim=True)
     cumval = torch.cumsum(val, dim=1)
-    patch_size = 8
+    patch_size = 4
     w_featmap = Tx3.shape[-2] // patch_size
     h_featmap = Tx3.shape[-1] // patch_size
     threshold = 0.6 # We visualize masks obtained by thresholding the self-attention maps to keep xx% of the mass.
@@ -115,21 +102,36 @@ while True:
     attentions_list.append(attentions_mean)
     frames_list.append(frame)
 
+    # Compute depth
+    input_batch = transform(img).to(device)
+    with torch.no_grad():
+        prediction = midas(input_batch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=(10*9, 10*16),
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+    output = prediction.cpu().numpy()
+    depth = cv2.normalize(output, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
+    depth_list.append(depth)
+
 ims = []
-ims2 = []
 figure = plt.figure()
 
 for i in range(0,len(attentions_list)):
-    figure.add_subplot(2,1,1)
+    figure.add_subplot(3,1,1)
     im = plt.imshow(frames_list[i], animated=True)
-    figure.add_subplot(2,1,2)
+    figure.add_subplot(3,1,2)
     im2 = plt.imshow(attentions_list[i], animated=True)
+    figure.add_subplot(3,1,3)
+    im3 = plt.imshow(depth_list[i], animated=True)
     # if i == 0:
     #     figure.add_subplot(2,1,1)
     #     plt.imshow(frames_list[i], animated=True)
     #     figure.add_subplot(2,1,2)
     #     plt.imshow(attentions_list[i], animated=True)
-    ims.append([im, im2])
+    ims.append([im, im2, im3])
 
 
 ani = animation.ArtistAnimation(figure, ims, blit=True, interval=50)
