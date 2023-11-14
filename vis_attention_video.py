bb0ea83f289805414be6cb748d831a67ee65b722
Brian Johnson <brjo8465@colorado.edu>
2023-11-14 16:56:27 +0100

    Updating automation/scalability, part 1

--- vis_attention_video.py
+++ vis_attention_video.py
@@ -20,17 +20,21 @@ from time import sleep
 device = torch.device("cpu")
 
 #model = timm.create_model('vit_small_patch16_224_dino',pretrained=True)
-model = torch.hub.load('facebookresearch/dino:main','dino_vits8')
+dino8 = torch.hub.load('facebookresearch/dino:main','dino_vits8')
+dino8.to(device)
+dino8.eval()
+dino16 = torch.hub.load('facebookresearch/dino:main','dino_vits16')
+dino16.to(device)
+dino16.eval()
 # model = torch.hub.load('facebookresearch/dinov2','dinov2_vits14')
-
-
-# Load MiDaS model onto CPU
-device = torch.device('cpu')
 # midas = torch.hub.load('intel-isl/MiDaS','DPT_Hybrid')
-midas = torch.hub.load('intel-isl/MiDaS','MiDaS_small')
+midas_s = torch.hub.load('intel-isl/MiDaS','MiDaS_small')
+midas_s.to(device)
+midas_s.eval()
+midas_h = torch.hub.load('intel-isl/MiDaS','DPT_Hybrid')
+midas_h.to(device)
+midas_h.eval()
 # midas = torch.hub.load('intel-isl/MiDaS', 'custom', path='../utilites/dpt_beit_large_512.pt', force_reload=True)
-midas.to(device)
-midas.eval()
 midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 transform = midas_transforms.small_transform
 
@@ -49,7 +53,8 @@ def get_last_self_attention(self, x, masks=None):
 
 
 # Setup frame capture
-cap = cv2.VideoCapture('video5.mp4') #use video
+video = 'boat'
+cap = cv2.VideoCapture("video_" + video + ".mp4") #use video
 #cap = cv2.VideoCapture(0) #stream from webcam
 previous_frame = None
 
@@ -58,6 +63,9 @@ attentions_list = []
 depth_list = []
 combined_list = []
 Hasel_list = []
+imsize = [15, 30, 45]
+attentions_mean = [[], [], []]
+depth = [[], [], []]
 
 while True: 
     # Load frame
@@ -69,103 +77,72 @@ while True:
     frame = img
     # img = cv2.addWeighted(img,1.5,img,0,1)
     
+    ### Compute attention ###    
     image = Image.fromarray(img)
-    Tx = transforms.Resize((50*9,50*16))(image)
-    Tx2 = transforms.ToTensor()(Tx).unsqueeze_(0)
-    Tx3 = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(Tx2)
-    Tx3.requires_grad = True
-
-    model.eval()
-    model.to(device)
-
-    start_time = time.time()
-    attentions = model.get_last_selfattention(Tx3)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    
-    nh = attentions.shape[1]
-    attentions = attentions[0, :, 0, 1:].reshape(nh,-1)
-    patch_size = 4
-    w_featmap = Tx3.shape[-2] // patch_size
-    h_featmap = Tx3.shape[-1] // patch_size
-
-    attentions = attentions.reshape(nh, w_featmap//2, h_featmap//2)
-    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=1, mode="nearest")[0].detach().numpy()
-    attentions_mean = np.mean(attentions, axis=0)
-
-
-    # Compute depth:
     input_batch = transform(img).to(device)
-    with torch.no_grad():
-        prediction = midas(input_batch)
-        prediction = torch.nn.functional.interpolate(
-            prediction.unsqueeze(1),
-            size=(20*9, 20*16),
-            mode="bicubic",
-            align_corners=False,
-        ).squeeze()
-    depth = prediction.cpu().numpy()
+    for ii in range(0,len(imsize)):
+        Tx = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(transforms.ToTensor()(transforms.Resize((imsize[ii]*9,imsize[ii]*16))(image)).unsqueeze_(0))
+        Tx.requires_grad = True
+
+        attentions = dino8.get_last_selfattention(Tx)
     
-    new_size = attentions_mean.shape
+        nh = attentions.shape[1]
+        attentions = attentions[0, :, 0, 1:].reshape(nh,-1)
+        patch_size = 4
+        w_featmap = Tx.shape[-2] // patch_size
+        h_featmap = Tx.shape[-1] // patch_size
 
-    depth_re = cv2.resize(depth, dsize=(new_size[1], new_size[0]), interpolation=cv2.INTER_CUBIC)
-    depth_nm = cv2.normalize(depth_re, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
-    attentions_nm = cv2.normalize(attentions_mean, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
+        attentions = attentions.reshape(nh, w_featmap//2, h_featmap//2)
+        attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=1, mode="nearest")[0].detach().numpy()
+        attentions_mean[ii] = np.mean(attentions, axis=0)
 
+    ### Compute depth ###
+    # start_time = time.time()
+    # print("--- %s seconds ---" % (time.time() - start_time))
+
+        with torch.no_grad():
+            prediction = midas_s(input_batch)
+            prediction = torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=(imsize[ii]*9, imsize[ii]*16),
+                mode="bicubic",
+                align_corners=False,
+            ).squeeze()
+        depth[ii] = prediction.cpu().numpy()
+    
+        new_size = attentions_mean[0].shape
+
+        depth_re = cv2.resize(depth[ii], dsize=(new_size[1], new_size[0]), interpolation=cv2.INTER_CUBIC)
+        depth_nm = cv2.normalize(depth_re, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
+        attentions_nm = cv2.normalize(attentions_mean[ii], None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
+
+    print(type(depth_re))
+    print(type(depth_nm))
     combined = depth_nm * attentions_nm
     combined = (combined > 0.1) * combined
     Hasel = cv2.resize(combined, dsize=(10, 6), interpolation=cv2.INTER_CUBIC)
 
-    # Update:
+    ### Update ###
     attentions_list.append(attentions_mean)
     depth_list.append(depth)
     frames_list.append(frame)
     combined_list.append(combined)
     Hasel_list.append(Hasel)
+    print(len(frames_list))
 
-ims = []
-figure = plt.figure()
-for i in range(0,len(frames_list)):
-    im = plt.imshow(attentions_list[i], animated=True)
-    ims.append([im])
-ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-ani.save('animation5_attention20.mp4', writer = 'ffmpeg', bitrate=1000, fps=15)
-plt.close()
+data_list = [frames_list, attentions_list, depth_list, combined_list, Hasel_list]
+name_list = ["frames", "attention", "depth", "combined", "HASEL"]
 
-ims = []
-figure = plt.figure()
-for i in range(0,len(frames_list)):
-    im = plt.imshow(frames_list[i], animated=True)
-    ims.append([im])
-ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-ani.save('animation5_frames.mp4', writer = 'ffmpeg', bitrate=1000, fps=15)
-plt.close()
+for i in range(0,len(data_list)):
+    print("Plotting %i list data" % (i))
+    ims = []
+    figure = plt.figure()
+    for j in range(0,len(frames_list)):
+        im = plt.imshow(data_list[i][j], animated=True)
+        ims.append([im])
+    ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
+    filename = "animation_" + video + "_" + name_list[i] + ".mp4"
+    ani.save(filename, writer = "ffmpeg", bitrate=1000, fps=15)
+    plt.close()
 
-ims = []
-figure = plt.figure()
-for i in range(0,len(frames_list)):
-    im = plt.imshow(depth_list[i], animated=True)
-    ims.append([im])
-ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-ani.save('animation5_depth20.mp4', writer = 'ffmpeg', bitrate=1000, fps=15)
-plt.close()
-
-ims = []
-figure = plt.figure()
-for i in range(0,len(frames_list)):
-    im = plt.imshow(combined_list[i], animated=True)
-    ims.append([im])
-ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-ani.save('animation5_combined2020.mp4', writer = 'ffmpeg', bitrate=1000, fps=15)
-plt.close()
-
-ims = []
-figure = plt.figure()
-for i in range(0,len(frames_list)):
-    im = plt.imshow(Hasel_list[i], animated=True)
-    ims.append([im])
-ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-ani.save('animation5_HASEL_10_6.mp4', writer = 'ffmpeg', bitrate=1000, fps=15)
-plt.close()
-
-# input('Ready to display')
-# plt.show()
+print("Finished!")
