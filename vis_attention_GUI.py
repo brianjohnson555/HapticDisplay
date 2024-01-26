a8dc2ae2929252ef4a2a1e85560be1fb0b0e946c
Brian Johnson <brjo8465@colorado.edu>
2024-01-26 10:20:08 +0100

    Add scale factor for combined step

--- vis_attention_GUI.py
+++ vis_attention_GUI.py
@@ -26,7 +26,9 @@ TKframe_entries = [] # Create empty frames for data entry slots
 for ii in range(0,5):
     TKframe_entries.append(tk.Frame(master=window))
 
-TKframe_buttons = [tk.Frame(master=window)] # Create empty frames for buttons
+TKframe_buttons = [] # Create empty frames for buttons
+for ii in range(0,4):
+    TKframe_buttons.append(tk.Frame(master=window))
 
 ## Place all frames in the grid
 TKframe_labels[0].grid(row=0, column=0, padx=5, pady=5)
@@ -78,8 +80,10 @@ cap = cv2.VideoCapture("video_" + video + ".mp4") #use video
 ################## HYPERPARAMETERS ##################
 FRAME_NUM = 1 # starting frame
 RESOLUTION_ATT = 150 # resolution of get_attention for DINO model
+MODEL = 'hybrid' # MiDaS model type ('small', 'hybrid', 'large')
 THRESHOLD_VAL = 0.25 # threshold of attention+depth combination
-BIAS = 0.75 # bias towards attention for attention+depth combination
+BIAS = 0.6 # bias towards attention for attention+depth combination
+SCALE = 4 # scaling of combined array (scale*[16, 9])
 DISPLAY_W = 10 # HASEL haptic display width (pixels)
 DISPLAY_H = 6 # HASEL haptic display height (pixels)
 ATTENTION = [] # will become torch tensor holding attention data
@@ -109,7 +113,7 @@ def get_attention():
                                 transforms.Resize((RESOLUTION_ATT,int(np.floor(RESOLUTION_ATT*1.7777)))),
                                 # transforms.CenterCrop(resolution), #should be multiple of model patch_size                 
                                 transforms.ToTensor(),                    
-                                transforms.Normalize(mean=0.5, std=0.2)
+                                #transforms.Normalize(mean=0.5, std=0.2)
                                 ])
     frame_t = transform1(frame).unsqueeze(0)
     attentions = dino8.get_last_selfattention(frame_t)
@@ -127,20 +131,29 @@ def get_attention():
 # get depth estimation of frame using MiDaS
 def get_depth():
     global IMG
+    global MODEL
     img = IMG
-    transform2 = midas_transforms.dpt_transform
-    frame_t = transform2(img)
-    depth = midas_s(frame_t)
+    if MODEL == 'small':
+        frame = midas_transforms.small_transform(img) # use small image transform
+        depth_size_x = 256
+        depth = midas_s(frame) # evaluate using small model
+    else:
+        frame = midas_transforms.dpt_transform(img) # use DPT image transform
+        depth_size_x = 672
+        if MODEL == 'hybrid':
+            depth = midas_h(frame) # evaluate using hybrid model
+        else:
+            depth = midas_l(frame) # evaluate using large model
+
     depth = depth.cpu().detach().numpy().squeeze(0)
     
-    ## EXPERIMENTAL: remove normal depth gradient from depth map
+    # remove normal depth gradient from depth map
     depth_nm = cv2.normalize(depth, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
-    xgrid = np.zeros(672, dtype=float)
-    # ygrid = np.logspace(-1, 0, num=384, dtype=float)
-    ygrid = depth_nm.mean(axis=1)
-    grad_array = np.meshgrid(xgrid, ygrid)[1]
+    xgrid = np.zeros(depth_size_x, dtype=float)
+    ygrid = depth_nm.mean(axis=1) # make mean-depth-based gradient
+    grad_array = np.meshgrid(xgrid, ygrid)[1] # form gradient array w/ same size as depth
     depth_sub = (depth_nm - grad_array)
-    depth_sub = (depth_sub > 0) * depth_sub
+    depth_sub = (depth_sub > 0) * depth_sub # take only positive values
 
     return depth, depth_sub
 
@@ -149,11 +162,12 @@ def get_combined(method='sum'):
     global BIAS
     global DEPTH
     global ATTENTION
+    global SCALE
     depth = DEPTH
     attention = ATTENTION
-    depth_re = cv2.resize(depth, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
+    depth_re = cv2.resize(depth, dsize=(16*SCALE, 9*SCALE), interpolation=cv2.INTER_CUBIC)
     depth_nm = cv2.normalize(depth_re, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
-    attention_re = cv2.resize(attention, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
+    attention_re = cv2.resize(attention, dsize=(16*SCALE, 9*SCALE), interpolation=cv2.INTER_CUBIC)
     attention_nm = cv2.normalize(attention_re, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_64F)
 
     if method=='multiply':
@@ -224,8 +238,9 @@ def update_attentions():
 # update depth plot with new results
 def update_depth():
     global DEPTH
+    global MODEL
     depth_before, DEPTH = get_depth()
-    plot_overlay(canvaslist[1], plotlist[1], depth_before, f"STEP2: Depth (model=Hybrid)")
+    plot_overlay(canvaslist[1], plotlist[1], depth_before, f"STEP2: Depth (model={MODEL})")
     plot_overlay(canvaslist[6], plotlist[6], DEPTH, f"STEP3: Depth correction")
 
 # update combined plot with new results
@@ -259,28 +274,22 @@ for fignum in range(0,len(figlist)):
 
 ################## TKINTER LABELS AND INPUTS ###################
 # Set up labels
-label1 = tk.Label(text="Advance frame", master=TKframe_labels[0])
-label1.pack()
-label2 = tk.Label(text="Set attention resolution", master=TKframe_labels[1])
-label2.pack()
-label3 = tk.Label(text="Set bias value (0-1)", master=TKframe_labels[2])
-label3.pack()
-label4 = tk.Label(text="Set threshold value (0-1)", master=TKframe_labels[3])
-label4.pack()
-label5 = tk.Label(text="Set display grid (W x H)", master=TKframe_labels[4])
-label5.pack()
+labels = []
+labels.append(tk.Label(text="Advance frame", master=TKframe_labels[0]))
+labels.append(tk.Label(text="Set attention resolution", master=TKframe_labels[1]))
+labels.append(tk.Label(text="Set bias value (0-1)\nSet scale factor", master=TKframe_labels[2]))
+labels.append(tk.Label(text="Set threshold value (0-1)", master=TKframe_labels[3]))
+labels.append(tk.Label(text="Set display grid (W x H)", master=TKframe_labels[4]))
+for label in range(0, len(labels)):
+    labels[label].pack()
 
 # Set up user input with entries
-entry1 = tk.Entry(width=5, master=TKframe_entries[0])
-entry1.pack()
-entry2 = tk.Entry(width=5, master=TKframe_entries[1])
-entry2.pack()
-entry3 = tk.Entry(width=5, master=TKframe_entries[2])
-entry3.pack()
-entry4 = tk.Entry(width=5, master=TKframe_entries[3])
-entry4.pack()
-entry5 = tk.Entry(width=5, master=TKframe_entries[4])
-entry5.pack()
+entry = []
+for entrynum in range(0, len(TKframe_entries)):
+    entry.append(tk.Entry(width=5, master=TKframe_entries[entrynum]))
+    entry[entrynum].pack()
+entry.append(tk.Entry(width=5, master=TKframe_entries[1]))
+entry[5].pack()
 
 # Advance frame button
 next_button = tk.Button(text="Next frame", master=TKframe_buttons[0])
@@ -288,75 +297,114 @@ next_button.pack()
 next_10_button = tk.Button(text="Next 10th frame", master=TKframe_buttons[0])
 next_10_button.pack()
 
+# MiDaS model select buttons (place in Attention entry frame)
+midas_l_button = tk.Button(text="MiDas Large", master=TKframe_entries[0])
+midas_l_button.pack()
+midas_h_button = tk.Button(text="MiDas Hybrid", master=TKframe_entries[0])
+midas_h_button.pack()
+midas_s_button = tk.Button(text="MiDas Small", master=TKframe_entries[0])
+midas_s_button.pack()
+
 ################## EVENT HANDLES ##################
 ### Do this when Enter key is pressed on the Entry item:
 # for attention resolution
-def handle_keypress_att(entry):
+def handle_keypress_att(entry1):
     global RESOLUTION_ATT
-    if not entry1.get(): # if user hasn't input anything, do nothing
+    if not entry[0].get(): # if user hasn't input anything, do nothing
         pass
     else:
-        RESOLUTION_ATT = int(entry1.get())
+        RESOLUTION_ATT = int(entry[0].get())
     update_attentions()
     update_combined()
     update_threshold()
     update_downsample()
 
 # for bias
-def handle_keypress_bias(entry):
+def handle_keypress_bias(entry1):
     global BIAS
-    if not entry2.get():
+    if not entry[1].get():
         pass
     else:
-        BIAS = float(entry2.get())
+        BIAS = float(entry[1].get())
+    update_combined()
+    update_threshold()
+    update_downsample()
+
+# for scale
+def handle_keypress_scale(entry1):
+    global SCALE
+    if not entry[5].get():
+        pass
+    else:
+        SCALE = int(entry[5].get())
     update_combined()
     update_threshold()
     update_downsample()
 
 # for threshold value
-def handle_keypress_thresh(entry):
+def handle_keypress_thresh(entry1):
     global THRESHOLD_VAL
-    if not entry3.get():
+    if not entry[2].get():
         pass
     else:
-        THRESHOLD_VAL = float(entry3.get())
+        THRESHOLD_VAL = float(entry[2].get())
     update_threshold()
     update_downsample()
 
 # for haptic display width
-def handle_keypress_disp_W(entry):
+def handle_keypress_disp_W(entry1):
     global DISPLAY_W
-    if not entry4.get():
+    if not entry[3].get():
         pass
     else:
-        DISPLAY_W = int(entry4.get())
+        DISPLAY_W = int(entry[3].get())
     update_downsample()
 
 # for haptic display height
-def handle_keypress_disp_H(entry):
+def handle_keypress_disp_H(entry1):
     global DISPLAY_H
-    if not entry5.get():
+    if not entry[4].get():
         pass
     else:
-        DISPLAY_H = int(entry5.get())
+        DISPLAY_H = int(entry[4].get())
     update_downsample()
 
 # bind all entries to the events
-entry1.bind("<Return>", handle_keypress_att)
-entry2.bind("<Return>", handle_keypress_bias)
-entry3.bind("<Return>", handle_keypress_thresh)
-entry4.bind("<Return>", handle_keypress_disp_W)
-entry5.bind("<Return>", handle_keypress_disp_H)
+entry[0].bind("<Return>", handle_keypress_att)
+entry[1].bind("<Return>", handle_keypress_bias)
+entry[2].bind("<Return>", handle_keypress_thresh)
+entry[3].bind("<Return>", handle_keypress_disp_W)
+entry[4].bind("<Return>", handle_keypress_disp_H)
+entry[5].bind("<Return>", handle_keypress_scale)
 
 # Do this when the "Next frame" button is pressed:
-def handle_buttonpress(event):
+def handle_buttonpressnext(event):
     update_frame(1)
-next_button.bind("<Button>", handle_buttonpress) # bind to Next Frame button
+next_button.bind("<Button>", handle_buttonpressnext) # bind to Next Frame button
 
-def handle_buttonpress(event):
+def handle_buttonpressnext10(event):
     update_frame(10)
-next_10_button.bind("<Button>", handle_buttonpress) # bind to Next 10th Frame button
+next_10_button.bind("<Button>", handle_buttonpressnext10) # bind to Next 10th Frame button
 
+# Do this when the MiDaS model buttons are pressed:
+
+def handle_buttonpress_midas_l(event):
+    global MODEL
+    MODEL = 'large'
+    update_depth()
+midas_l_button.bind("<Button>", handle_buttonpress_midas_l) # bind to Next Frame button
+
+def handle_buttonpress_midas_h(event):
+    global MODEL
+    MODEL = 'hybrid'
+    update_depth()
+midas_h_button.bind("<Button>", handle_buttonpress_midas_h) # bind to Next Frame button
+
+def handle_buttonpress_midas_s(event):
+    global MODEL
+    MODEL = 'small'
+    update_depth()
+midas_s_button.bind("<Button>", handle_buttonpress_midas_s) # bind to Next Frame button
 
 ################## RUN TKINTER ##################
 window.mainloop()
