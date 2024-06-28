da09bd5908b40d70f7e581c72966a33e87e69e90
Brian Johnson <brjo8465@colorado.edu>
2024-06-28 11:05:35 +0200

    Demos working

--- DEMO_koi.py
+++ DEMO_koi.py
@@ -15,6 +15,7 @@ import urllib.request
 import serial
 from numpy import genfromtxt
 
+video = 'koi'
 ###### MAIN ######
 # Set up pixel to switchboard mapping:
 # bot: |2|4|6|8|10|
@@ -22,12 +23,12 @@ from numpy import genfromtxt
 # top: |2|4|6|8|10|
 # top: |1|3|5|7|9|
 def map_pixels(output):
-    periods_bot = np.array([output[1,0], output[0,0], output[1,1], output[0,1], output[1,2], output[0,2], output[1,3], output[0,3], output[1,4], output[0,4]])
-    periods_top = np.array([output[3,0], output[2,0], output[3,1], output[2,1], output[3,2], output[2,2], output[3,3], output[2,3], output[3,4], output[2,4]])
+    periods_bot = np.array([output[5], output[0], output[6], output[1], output[7], output[2], output[8], output[3], output[9], output[4]])
+    periods_top = np.array([output[15], output[10], output[16], output[11], output[17], output[12], output[18], output[13], output[19], output[14]])
     return periods_bot, periods_top
 
 # Load data
-data=genfromtxt(FILENAME,delimiter=',')
+data=genfromtxt(FILENAME,delimiter=',')[:,0:20]
 
 # Set up serial list
 ser = [serial.Serial(COM1, 9600, timeout=0, bytesize=serial.EIGHTBITS),
@@ -46,29 +47,51 @@ ser[0].write('E'.encode()) # Enable HV
 ser[1].write('E'.encode()) # Enable HV
 time.sleep(0.25)
 
+
 while True:
-    periods = []
-    packets = []
+    isClose=False
+    cap = cv2.VideoCapture("video_" + video + ".mp4") #use video
+    dataindex = 0
+    while True:
+        ret,img = cap.read()
+        if(ret==True):
+            periods = []
+            packets = []
+            
+            output = data[dataindex,:] # Read each line of data
+            if dataindex>1798:
+                dataindex=0 # loop back to start when you run out of data
 
-    output = data # Need to read each line of data, and loop when it runs out
+            ## LINEAR MAPPING FROM INTENSITY TO FREQUENCY (TO DISPLAY)
+            mapped_freq = 30*output # mapped frequency (Hz)
+            mapped_freq[mapped_freq==0] = 0.01
+            mapped_per = np.reciprocal(mapped_freq) # mapped period (sec)
+            mapped_per_ms = 1000*mapped_per # mapped period (ms)
+            mapped_per_ms = mapped_per_ms.astype(int)
+            mapped_per_ms[mapped_per_ms>500] = 0 # anything above 500 ms = 0 (below 2 Hz = 0)
 
-    ## LINEAR MAPPING FROM INTENSITY TO FREQUENCY (TO DISPLAY)
-    mapped_freq = 20*output # mapped frequency (Hz)
-    mapped_freq[mapped_freq==0] = 0.01
-    mapped_per = np.reciprocal(mapped_freq) # mapped period (sec)
-    mapped_per_ms = 1000*mapped_per # mapped period (ms)
-    mapped_per_ms = mapped_per_ms.astype(int)
-    mapped_per_ms[mapped_per_ms>500] = 0 # anything above 500 ms = 0 (below 2 Hz = 0)
+            periods_bot, periods_top = map_pixels(mapped_per_ms)
+            packet_bot = make_packet(periods_bot)
+            packet_top = make_packet(periods_top)
+            ser[0].write(packet_bot)
+            ser[1].write(packet_top)
 
-    periods_bot, periods_top = map_pixels(mapped_per_ms)
-    packet_bot = make_packet(periods_bot)
-    packet_top = make_packet(periods_top)
-    ser[0].write(packet_bot)
-    ser[1].write(packet_top)
-    time.sleep(0.05)
+            cv2.namedWindow('Video',cv2.WINDOW_KEEPRATIO)
+            cv2.imshow('Video',img)
 
-    if(cv2.waitKey(10) & 0xFF == ord('b')):
+            dataindex += 1
+
+            # time.sleep(0.05)
+            if(cv2.waitKey(10) & 0xFF == ord('b')):
+                isClose = True
+                break
+        else:  
+            break
+    if isClose:
         break
+
+        
+        
     
 
 ser[0].write('D'.encode()) # disable HV  
