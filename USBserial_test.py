algo_func bug fix, global normalization processing

USBserial_test.py
@@ -20,7 +20,7 @@ def make_packets(duties, periods):
     packetlist = []
     packetlist.append(('T').encode()) # encode start of period array
     for period in periods:
-        packetlist.append((2*period.item()).to_bytes(2, byteorder='little')) # convert to 16bit, factor of 2 for period in MCU
+        packetlist.append((period.item()).to_bytes(2, byteorder='little')) # convert to 16bit, factor of 2 for period in MCU
     packet_period = b''.join(packetlist) # combine packetlist as bytes
 
     return packet_duty, packet_period
@@ -28,8 +28,8 @@ def make_packets(duties, periods):
 ser.write('E'.encode()) # Enable HV
 time.sleep(0.5)
 
-periods = np.array([4000, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-duties = np.array([0.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+periods = np.array([2000, 0, 0, 0, 0, 0, 0, 0, 0, 10])
+duties = np.array([0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 
 packet_duty, packet_period = make_packets(duties, periods)

vis_attention_processing_nonormal.py
@@ -149,7 +149,7 @@ def get_downsample(thresholded):
 
 
 print("Running...")
-while frame_num<600: 
+while True: 
         # grab next frame from video
         frame, img = grab_frame(cap)
         if frame is None:
@@ -190,36 +190,72 @@ while frame_num<600:
 
         frame_num += 1
 
-print("Plotting...")
-data_list = [output_list, depth_list, attention_list]
+print("Global scaling...")
+attention_np = np.array(attention_list)
+flat = attention_np.flatten()
+scale_att = 1/(np.mean(flat)+3*np.std(flat))
 
-import pickle
+depth_np = np.array(depth_list)
+flat = depth_np.flatten()
+scale_dep = 1/(np.mean(flat)+3*np.std(flat))
 
-with open('data_saved.pickle', 'wb') as handle:
-    pickle.dump(data_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
+output_list_scaled = []
+BIAS = 0.9
 
+for frame in range(len(attention_list)):
+    attention_nm = attention_list[frame]*scale_att
+    depth_nm = depth_list[frame]*scale_dep
 
-# Loading the file:
-# file = open("data_saved.pickle",'rb')
-# object_file = pickle.load(file)
-# file.close()
+    depth_re = cv2.resize(depth_nm, dsize=(16*SCALE, 9*SCALE), interpolation=cv2.INTER_CUBIC)
+    attention_re = cv2.resize(attention_nm, dsize=(16*SCALE, 9*SCALE), interpolation=cv2.INTER_CUBIC)
 
-# ims = []
-# figure = plt.figure()
-# for i in range(0,len(output_list)):
-#     im = plt.imshow(output_list[i], animated=True)
-#     ims.append([im])
-# ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
-# filename = "animation_" + video + "_output.mp4"
-# ani.save(filename, writer = "ffmpeg", bitrate=1000, fps=30)
-# plt.close()
+    # combined = ((1-BIAS)*depth_re + (BIAS)*attention_re)
+    combined = depth_re*attention_re
+    thresholded = (combined > THRESHOLD_VAL) * combined
 
-# filename_data = "data" + video + "_output.txt"
-# with open(filename_data, 'w') as fo:
-#     for idx, item in enumerate(output_list):
-#         for row in range(DISPLAY_H):
-#             for column in range(DISPLAY_W):
-#                 fo.write(str(item[row, column]) + ', ')
-#         fo.write('\n')
+    ### new code for interpolation:
+    downsampled = np.zeros((DISPLAY_H, DISPLAY_W))
+    interval_H = int(np.floor(thresholded.shape[0]/DISPLAY_H))
+    interval_W = int(np.floor(thresholded.shape[1]/DISPLAY_W))
+    for rr in range(0, DISPLAY_H):
+        for cc in range(0, DISPLAY_W):
+            frame_slice = thresholded[rr*interval_H:(rr+1)*interval_H, cc*interval_W:(cc+1)*interval_W]
+            mean_slice = np.mean(frame_slice)
+            std_slice = np.std(frame_slice)
+            max_slice = np.max(frame_slice)
+            if mean_slice+3*std_slice > max_slice: # I'm doing some weird selection of max vs. mean depending on std of the frame slice
+                downsampled[rr, cc] = max_slice
+            else:
+                downsampled[rr, cc] = mean_slice
+
+    output_list_scaled.append(downsampled)
+
+output_np = np.array(output_list_scaled)
+flat = output_np.flatten()
+max_thresh = (np.mean(flat)+5*np.std(flat))
+output_np[output_np>max_thresh] = max_thresh
+scale_output = 1/(max_thresh)
+output_scale = output_np*scale_output
+
+for frame in range(len(output_list_scaled)):
+    output_list_scaled[frame] = output_scale[frame]
+
+ims = []
+figure = plt.figure()
+for i in range(0,len(output_list_scaled)):
+    im = plt.imshow(output_list[i], animated=True)
+    ims.append([im])
+ani = animation.ArtistAnimation(figure, ims, blit=True, repeat=False)
+filename = "animation_" + video + "_output_scaled.mp4"
+ani.save(filename, writer = "ffmpeg", bitrate=1000, fps=30)
+plt.close()
+
+filename_data = "data" + video + "_output_scaled.txt"
+with open(filename_data, 'w') as fo:
+    for idx, item in enumerate(output_list_scaled):
+        for row in range(DISPLAY_H):
+            for column in range(DISPLAY_W):
+                fo.write(str(item[row, column]) + ', ')
+        fo.write('\n')
 
 print("Finished!")